01be78fbeb958707d1a635271398895b3dd723a2
RODOLPHO MAGALHAES <rodolphogasper@yahoo.com.br>
2026-02-26 16:28:18 -0300

    Novas atualizações snapshot

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,16 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  timeout: 5000,
+
+  // Tempo m[áximo para cada teste completo (30 segundos é o padrão)
+  timeout: 60_000,
+
+  // Tempo máximo para assertions (toBeVisible(), toHaveText(), etc.)
+expect: {
+  timeout: 5_000
+},
+
+
   testDir: './playwright/e2e',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -31,6 +40,15 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+
+
+    // Tempo máximo para actions (click, fill, etc.)
+    // Quando valor 0 herda o limite do timeout geral do teste
+    actionTimeout: 5_000,
+
+// Tempo máximo para navegação (goto, waitforURL, etc.)
+// Quando valor 0 herda o limite do timeout geral do teste
+    navigationTimeout: 10_000,
   },
 
   /* Configure projects for major browsers */
--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -4,7 +4,11 @@ import { test, expect } from '@playwright/test'
 /// PAV - Preparar, Agir, Validar
 
 test('deve consultar um pedido aprovado', async ({ page }) => { // Async garante que cada step seja executado de forma assíncrona
-    // Arrange
+    
+  //Test Data
+  const order = 'VLO-D2JOOV'
+
+  // Arrange
   await page.goto('http://localhost:5173/') // GO TOgarante carregra a página
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint') // EXPECT Garantir que tenha a descrição
 
@@ -12,7 +16,7 @@ test('deve consultar um pedido aprovado', async ({ page }) => { // Async garante
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
   // Act
-  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill('VLO-D2JOOV')
+  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order)
   //await page.getByTestId('search-order-button').click() --alterado para o novo nome do botão
   await page.getByRole('button', { name: 'Buscar Pedido' }).click()
   
@@ -21,11 +25,33 @@ test('deve consultar um pedido aprovado', async ({ page }) => { // Async garante
   // await expect(page.getByTestId('order-result-id')).toBeVisible({timeout: 5_000}) // timeout explicita para  exemplo10 segundos melhor método para espera
   // await expect(page.getByTestId('order-result-id')).toContainText('VLO-D2JOOV')
   
-
   // await expect(page.getByTestId('order-result-status')).toBeVisible()
   // await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
-  await expect(page.getByText('VLO-D2JOOV')).toBeVisible()
+  await expect(page.getByText(order)).toBeVisible()
   await expect(page.getByText('APROVADO')).toBeVisible()
   
+})
+
+
+test('deve exibir mensagem quando o pedido não é encontrado', async ({ page }) => { 
+    
+
+  const order = 'VLO-ABC123'
+
+  
+  await page.goto('http://localhost:5173/') 
+  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint') 
+
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click() 
+  await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
+
+  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order)
+  await page.getByRole('button', { name: 'Buscar Pedido' }).click()
+
+  await expect(page.locator('#root')).toMatchAriaSnapshot(`
+    - img
+    - heading "Pedido não encontrado" [level=3]
+    - paragraph: Verifique o número do pedido e tente novamente
+    `)
 
 })
